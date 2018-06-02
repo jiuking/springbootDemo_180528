803015078abf366501fb610d8cf20f3421d46284
hjc <lingshao2010@126.com>
2018-06-02 17:13:29 +0800

    spring boot 添加定时任务

---
+++ src/main/java/com/hjc/demo/config/SchedulingConfig.java
@@ -0,0 +1,26 @@
+package com.hjc.demo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * 定时任务配置类
+ *
+ * @author Hjc
+ * @date 2018-06-02 16:59:00
+ */
+@Configuration
+@EnableScheduling//启动定时任务
+public class SchedulingConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(SchedulingConfig.class);
+
+    @Scheduled(cron = "0/20 * * * * ?")
+    public void scheduled() {
+        logger.info("----->定时任务<-----");
+    }
+
+}
